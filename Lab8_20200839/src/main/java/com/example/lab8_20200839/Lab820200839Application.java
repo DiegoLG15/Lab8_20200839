91f02eed537df72a26302de61dca4bab3b5168a0
DiegoLG15 <a20200839@pucp.edu.pe>
2023-06-13 20:40:59 -0500

    first commit

---
+++ Lab8_20200839/src/main/java/com/example/lab8_20200839/Lab820200839Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20200839;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab820200839Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab820200839Application.class, args);
+	}
+
+}
---
+++ Lab8_20200839/src/test/java/com/example/lab8_20200839/Lab820200839ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20200839;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab820200839ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
